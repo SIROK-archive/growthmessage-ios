a6e529f812e9056326ade088e49696bec88f4b93
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2015-03-02 18:25:53 +0900

    Add empty sample project

---
+++ sample/GrowthMessageSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GrowthMessageSample
+//
+//  Created by Naoyuki Kataoka on 2015/03/02.
+//  Copyright (c) 2015年 SIROK, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sample/GrowthMessageSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GrowthMessageSample
+//
+//  Created by Naoyuki Kataoka on 2015/03/02.
+//  Copyright (c) 2015年 SIROK, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

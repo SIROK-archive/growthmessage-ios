228dd698693c5a744809def45b926d23b8595efa
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2015-03-02 18:32:48 +0900

    Implement GrowthMessage process in sample project

--- sample/GrowthMessageSample/AppDelegate.h
+++ sample/GrowthMessageSample/AppDelegate.h
@@ -7,6 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <GrowthbeatCore/GrowthbeatCore.h>
+#import <GrowthMessage/GrowthMessage.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
--- sample/GrowthMessageSample/ViewController.m
+++ sample/GrowthMessageSample/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <GrowthMessage/GrowthMessage.h>
 
 @interface ViewController ()
 
@@ -14,14 +15,9 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [[GrowthMessage sharedInstance] openMessageIfAvailable];
 }
 
 @end

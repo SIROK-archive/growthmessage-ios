e71bba2efe6543b251d4decc6e1ea82a63392529
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2015-03-02 17:58:12 +0900

    Add GMMessage class

---
+++ source/GrowthMessage/GMMessage.h
@@ -0,0 +1,13 @@
+//
+//  GMMessage.h
+//  GrowthMessage
+//
+//  Created by Naoyuki Kataoka on 2015/03/02.
+//  Copyright (c) 2015年 SIROK, Inc. All rights reserved.
+//
+
+#import "GBDomain.h"
+
+@interface GMMessage : GBDomain
+
+@end
---
+++ source/GrowthMessage/GMMessage.m
@@ -0,0 +1,13 @@
+//
+//  GMMessage.m
+//  GrowthMessage
+//
+//  Created by Naoyuki Kataoka on 2015/03/02.
+//  Copyright (c) 2015年 SIROK, Inc. All rights reserved.
+//
+
+#import "GMMessage.h"
+
+@implementation GMMessage
+
+@end

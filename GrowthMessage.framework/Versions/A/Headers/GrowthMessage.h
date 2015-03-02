6b3a8166e6d67da2ce1bb6668d56b0191610e8e2
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2015-03-02 17:37:30 +0900

    Add aggregation target to package universal framework

---
+++ GrowthMessage.framework/Versions/A/Headers/GrowthMessage.h
@@ -0,0 +1,13 @@
+//
+//  GrowthMessage.h
+//  GrowthMessage
+//
+//  Created by Naoyuki Kataoka on 2015/03/02.
+//  Copyright (c) 2015年 SIROK, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GrowthMessage : NSObject
+
+@end

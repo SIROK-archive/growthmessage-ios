127f4a1647e61d14c8173a0011858864fa775eab
堀内 暢之 <horiuchinobuyuki@gmail.com>
2015-03-03 16:21:15 +0900

    sample code which will show one plain alert view (without networkings)

--- sample/GrowthMessageSample/ViewController.m
+++ sample/GrowthMessageSample/ViewController.m
@@ -8,8 +8,16 @@
 
 #import "ViewController.h"
 #import <GrowthMessage/GrowthMessage.h>
+#import <GrowthMessage/GMBasicMessageHandler.h>
+#import <GrowthMessage/GMButton.h>
 
-@interface ViewController ()
+@interface GrowthMessage (dev) {
+	
+}
+- (void)openMessage:(GMMessage*)message;
+@end
+
+@interface ViewController () <GrowthMessageDelegate>
 
 @end
 
@@ -17,7 +25,34 @@
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
+	[[GrowthMessage sharedInstance] setDelegate:self];
+	[[GrowthMessage sharedInstance] setMessageHandlers:
+	 [NSArray arrayWithObjects:
+	  [[GMBasicMessageHandler alloc] init]
+	  , nil]];
     [[GrowthMessage sharedInstance] openMessageIfAvailable];
+	
+	
+	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+		GMMessage *dummyMessage = [[GMMessage alloc] init];
+		dummyMessage.token = @"1234567abcdefg";
+		dummyMessage.type = @"plain";
+		dummyMessage.title = @"title";
+		dummyMessage.body = @"some text";
+		NSMutableArray *buttons = [NSMutableArray array];
+		{
+			GMButton *button = [[GMButton alloc] init];
+			[button setLabel:@"button label"];
+			[buttons addObject:button];
+		}
+		dummyMessage.buttons = buttons;
+		
+		[[GrowthMessage sharedInstance] openMessage:dummyMessage];
+	});
+}
+
+- (BOOL)shoudShowMessage:(GMMessage *)message {
+	return YES;
 }
 
 @end

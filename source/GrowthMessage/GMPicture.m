932ea28294d97e866c89ee9921aa0fc9daf22ea1
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2015-04-20 01:17:25 +0900

    Add GMPicture class and GMPictureExtension type

---
+++ source/GrowthMessage/GMPicture.m
@@ -0,0 +1,22 @@
+//
+//  GMPicture.m
+//  GrowthMessage
+//
+//  Created by Naoyuki Kataoka on 2015/04/20.
+//  Copyright (c) 2015年 SIROK, Inc. All rights reserved.
+//
+
+#import "GMPicture.h"
+
+@implementation GMPicture
+
+@synthesize id;
+@synthesize applicationId;
+@synthesize extension;
+@synthesize width;
+@synthesize height;
+@synthesize name;
+@synthesize created;
+@synthesize updated;
+
+@end

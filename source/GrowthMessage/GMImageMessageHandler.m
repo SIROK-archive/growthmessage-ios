2696c4274240b8b689de73bc80c675920017fae2
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2015-04-21 03:06:14 +0900

    Bind button actions

--- source/GrowthMessage/GMImageMessageHandler.m
+++ source/GrowthMessage/GMImageMessageHandler.m
@@ -12,7 +12,31 @@
 #import "GMCloseButton.h"
 #import "GMImageButton.h"
 
+@interface GMImageMessageHandler () {
+
+    NSMutableDictionary *boundMessages;
+    NSMutableDictionary *boundButtons;
+
+}
+
+@property (nonatomic, strong) NSMutableDictionary *boundMessages;
+@property (nonatomic, strong) NSMutableDictionary *boundButtons;
+
+@end
+
 @implementation GMImageMessageHandler
+
+@synthesize boundMessages;
+@synthesize boundButtons;
+
+- (instancetype) init {
+    self = [super init];
+    if (self) {
+        self.boundMessages = [[NSMutableDictionary alloc] init];
+        self.boundButtons = [[NSMutableDictionary alloc] init];
+    }
+    return self;
+}
 
 #pragma mark --
 #pragma mark GMMessageHandler
@@ -28,15 +52,15 @@
     }
 
     GMImageMessage *imageMessage = (GMImageMessage *)message;
-    
+
     NSArray *screenButtons = [self extractButtonsWithType:GMButtonTypeScreen imageMessage:imageMessage];
     GMScreenButton *screenButton = nil;
     if ([screenButtons count] > 0) {
         screenButton = [screenButtons objectAtIndex:0];
     }
-    
+
     NSArray *imageButtons = [self extractButtonsWithType:GMButtonTypeImage imageMessage:imageMessage];
-    
+
     NSArray *closeButtons = [self extractButtonsWithType:GMButtonTypeClose imageMessage:imageMessage];
     GMCloseButton *closeButton = nil;
     if ([closeButtons count] > 0) {
@@ -53,27 +77,31 @@
     } else {
         width = imageMessage.picture.width * height / imageMessage.picture.height;
     }
-    
+
     CGFloat ratio = width / imageMessage.picture.width;
 
     CGFloat left = (window.frame.size.width - width) / 2;
     CGFloat top = (window.frame.size.height - height) / 2;
-    
+
     UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:imageMessage.picture.url]]];
-    
+
     UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(left, top, width, height)];
     imageView.image = image;
     imageView.contentMode = UIViewContentModeScaleAspectFit;
+    imageView.userInteractionEnabled = YES;
     [window addSubview:imageView];
-    
+
     if (screenButton) {
         UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
         [button setImage:image forState:UIControlStateNormal];
         button.contentMode = UIViewContentModeScaleAspectFit;
         button.frame = CGRectMake(0, 0, width, height);
+        [boundMessages setObject:imageMessage forKey:[NSValue valueWithNonretainedObject:button]];
+        [boundButtons setObject:button forKey:[NSValue valueWithNonretainedObject:button]];
+        [button addTarget:self action:@selector(tapButton:) forControlEvents:UIControlEventTouchUpInside];
         [imageView addSubview:button];
     }
-    
+
     CGFloat imageButtonTop = height;
     for (GMImageButton *imageButton in [imageButtons reverseObjectEnumerator]) {
         CGFloat imageButtonWidth = imageButton.picture.width * ratio;
@@ -85,27 +113,43 @@
         [button setImage:imageButtonImage forState:UIControlStateNormal];
         button.contentMode = UIViewContentModeScaleAspectFit;
         button.frame = CGRectMake(imageButtonLeft, imageButtonTop, imageButtonWidth, imageButtonHeight);
+        [boundMessages setObject:imageMessage forKey:[NSValue valueWithNonretainedObject:button]];
+        [boundButtons setObject:imageButton forKey:[NSValue valueWithNonretainedObject:button]];
+        [button addTarget:self action:@selector(tapButton:) forControlEvents:UIControlEventTouchUpInside];
         [imageView addSubview:button];
     }
-    
+
     if (closeButton) {
         // TODO implement close button
     }
-    
+
     return YES;
 
 }
 
-- (NSArray *)extractButtonsWithType:(GMButtonType)type imageMessage:(GMImageMessage *)imageMessage {
-    
+- (NSArray *) extractButtonsWithType:(GMButtonType)type imageMessage:(GMImageMessage *)imageMessage {
+
     NSMutableArray *buttons = [NSMutableArray array];
-    
-    for (GMButton *button in imageMessage.buttons)
-        if(button.type == type)
+
+    for (GMButton *button in imageMessage.buttons) {
+        if (button.type == type) {
             [buttons addObject:button];
-    
+        }
+    }
+
     return buttons;
-    
+
+}
+
+- (void) tapButton:(id)sender {
+
+    GMMessage *message = [boundMessages objectForKey:[NSValue valueWithNonretainedObject:sender]];
+    GMButton *button = [boundButtons objectForKey:[NSValue valueWithNonretainedObject:sender]];
+
+    [[GrowthMessage sharedInstance] selectButton:button message:message];
+
+    // TODO Release bound buttons
+
 }
 
 @end

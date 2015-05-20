63e0137641d06e3c93693a6e9710ceed5f41fbd4
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2015-05-20 22:57:53 +0900

    Refactor GMImageMessageRenderer to support rotation

--- source/GrowthMessage/GMImageMessageRenderer.m
+++ source/GrowthMessage/GMImageMessageRenderer.m
@@ -12,11 +12,6 @@
 #import "GMImageButton.h"
 
 @interface GMImageMessageRenderer () {
-enum viewObjectName {
-    imageViewObject,
-    buttonViewObject,
-    closeViewObject
-};
 
     NSMutableDictionary *boundButtons;
     NSMutableDictionary *cachedImages;
@@ -48,13 +43,10 @@ enum viewObjectName {
         self.boundButtons = [NSMutableDictionary dictionary];
         self.cachedImages = [NSMutableDictionary dictionary];
     }
-    
+
     [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(resize)
-                                                 name:UIDeviceOrientationDidChangeNotification
-                                               object:nil];
-    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(show) name:UIDeviceOrientationDidChangeNotification object:nil];
+
     return self;
 }
 
@@ -74,10 +66,9 @@ enum viewObjectName {
     CGRect rect = CGRectMake(left, top, width, height);
 
     [view removeFromSuperview];
-    
+
     self.view = [[UIView alloc] initWithFrame:window.frame];
     view.backgroundColor = [UIColor colorWithWhite:0 alpha:0.5];
-    view.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
     [window addSubview:view];
 
     self.activityIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
@@ -96,47 +87,7 @@ enum viewObjectName {
         self.activityIndicatorView = nil;
 
     }];
-}
 
-- (void) resize {
-    UIWindow *window = [[[UIApplication sharedApplication] delegate] window];
-    
-    CGFloat availableWidth = MIN(imageMessage.picture.width, window.frame.size.width * 0.85);
-    CGFloat availableHeight = MIN(imageMessage.picture.height, window.frame.size.height * 0.85);
-    CGFloat ratio = MIN(availableWidth / imageMessage.picture.width, availableHeight / imageMessage.picture.height);
-    
-    CGFloat width = imageMessage.picture.width * ratio;
-    CGFloat height = imageMessage.picture.height * ratio;
-    CGFloat left = (window.frame.size.width - width) / 2;
-    CGFloat top = (window.frame.size.height - height) / 2;
-    
-    CGRect rect = CGRectMake(left, top, width, height);
-    
-    UIView *object;
-    for (int i = 0; i < [self.view.subviews count]; i++) {
-        
-        object = [self.view.subviews objectAtIndex:i];
-        
-        if (object.tag == imageViewObject) {
-            [object setFrame:rect];
-        }else if (object.tag == buttonViewObject) {
-            NSArray *imageButtons = [self extractButtonsWithType:GMButtonTypeImage];
-            CGFloat top_b = rect.origin.y + rect.size.height;
-            for (GMImageButton *imageButton in [imageButtons reverseObjectEnumerator]) {
-                CGFloat width_b = imageButton.picture.width * ratio;
-                CGFloat height_b = imageButton.picture.height * ratio;
-                CGFloat left_b = rect.origin.x + (rect.size.width - width_b) / 2;
-                object.frame = CGRectMake(left_b, i == 1 ? top_b - ( height_b * ratio ) : top_b - ( height_b  * ratio * 2 ), width_b, height_b);
-            }
-        }else if (object.tag == closeViewObject) {
-            GMCloseButton *closeButton = [[self extractButtonsWithType:GMButtonTypeClose] lastObject];
-            CGFloat width_c = closeButton.picture.width * ratio;
-            CGFloat height_c = closeButton.picture.height * ratio;
-            CGFloat left_c = rect.origin.x + rect.size.width - width_c / 2;
-            CGFloat top_c = rect.origin.y - height_c / 2;
-            object.frame = CGRectMake(left_c, top_c, width_c, height_c);
-        }
-    }
 }
 
 - (void) showImageWithRect:(CGRect)rect ratio:(CGFloat)ratio {
@@ -186,7 +137,6 @@ enum viewObjectName {
         [button setImage:[cachedImages objectForKey:imageButton.picture.url] forState:UIControlStateNormal];
         button.contentMode = UIViewContentModeScaleAspectFit;
         button.frame = CGRectMake(left, top, width, height);
-        button.tag = buttonViewObject;
         [button addTarget:self action:@selector(tapButton:) forControlEvents:UIControlEventTouchUpInside];
         [view addSubview:button];
 
@@ -213,7 +163,6 @@ enum viewObjectName {
     [button setImage:[cachedImages objectForKey:closeButton.picture.url] forState:UIControlStateNormal];
     button.contentMode = UIViewContentModeScaleAspectFit;
     button.frame = CGRectMake(left, top, width, height);
-    button.tag = closeViewObject;
     [button addTarget:self action:@selector(tapButton:) forControlEvents:UIControlEventTouchUpInside];
     [view addSubview:button];
 
@@ -259,11 +208,11 @@ enum viewObjectName {
                 continue;
         }
     }
-    
+
     for (NSString *urlString in urlStrings) {
         [self cacheImageWithUrlString:urlString completion:^(NSString *urlString){
             [urlStrings removeObject:urlString];
-            if([urlStrings count] == 0 && callback) {
+            if ([urlStrings count] == 0 && callback) {
                 callback();
             }
         }];
@@ -272,9 +221,18 @@ enum viewObjectName {
 }
 
 - (void) cacheImageWithUrlString:(NSString *)urlString completion:(void (^)(NSString *urlString))completion {
-
+    
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         
+        if([cachedImages objectForKey:urlString]) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (completion) {
+                    completion(urlString);
+                }
+            });
+            return;
+        }
+
         UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:urlString]]];
 
         dispatch_async(dispatch_get_main_queue(), ^{
